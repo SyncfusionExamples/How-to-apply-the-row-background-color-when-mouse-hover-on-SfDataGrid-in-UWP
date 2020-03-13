f6ab66f96626befe3d1d7efa7cc234412211466b
Karthik Raja <Karthikraja.kalaimani@syncfusion.com>
2020-03-13 18:02:24 +0530

    sample done and committed

--- DataGrid_Demo/DataGridDemo/DataGridDemo.UWP/CustomStackLayoutRenderer.cs
+++ DataGrid_Demo/DataGridDemo/DataGridDemo.UWP/CustomStackLayoutRenderer.cs
@@ -1,49 +1,22 @@
-# How-to-apply-the-row-background-color-when-mouse-hover-on-SfDataGrid-in-UWP
+﻿using DataGridDemo;
+using DataGridDemo.UWP;
+using Syncfusion.SfDataGrid.XForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Xamarin.Forms.Platform.UWP;
+using System.Reflection;
+using System.Diagnostics;
 
-## About the sample
-This example illustrates how to apply the row background color when mouse hover on SfDataGrid in UWP platform.
-
-By default, the SfDataGrid does not have a support to apply the row background colour when hover on the row in UWP platform. But we can do this by hosting the SfDataGrid into the custom StackLayout and then write the renderer for the custom stackLayout to detect the movement of the mouse pointer and apply the Background color whenever the mouse pointer moved over the Row of the SfDataGrid in PointerMoved Event of custom stackLayout and setting the previous background color to the row when mouse pointer is released from the current row.
-
-```XAML
-<?xml version="1.0" encoding="utf-8" ?>
-<ContentPage xmlns="http://xamarin.com/schemas/2014/forms"
-             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
-             xmlns:local="clr-namespace:DataGridDemo"
-             xmlns:data="clr-namespace:Syncfusion.Data;assembly=Syncfusion.Data.Portable"
-             xmlns:sfgrid="clr-namespace:Syncfusion.SfDataGrid.XForms;assembly=Syncfusion.SfDataGrid.XForms"
-             xmlns:sfPager="clr-namespace:Syncfusion.SfDataGrid.XForms.DataPager;assembly=Syncfusion.SfDataGrid.XForms"
-             x:Class="DataGridDemo.MainPage">
-    <ContentPage.Content>
-        <local:CustomStackLayout Orientation="Vertical">
-            <sfgrid:SfDataGrid x:Name="sfgrid" AllowResizingColumn="True" AllowSwiping="True">
-
-            </sfgrid:SfDataGrid>
-        </local:CustomStackLayout>
-
-    </ContentPage.Content>
-</ContentPage>
-```
- 
-
-```C#
-public class CustomStackLayout : StackLayout
-    {
-        public CustomStackLayout()
-        {
-
-        }
-
-        protected override void LayoutChildren(double x, double y, double width, double height)
-        {
-            base.LayoutChildren(x, y, width, height);
-        }
-    }
-```
-
-In UWP Renderer
-```C#
-public class CustomStackLayoutRenderer : VisualElementRenderer<CustomStackLayout, Control>
+[assembly: ExportRenderer(typeof(CustomStackLayout), typeof(CustomStackLayoutRenderer))]
+namespace DataGridDemo.UWP
+{
+   public class CustomStackLayoutRenderer : VisualElementRenderer<CustomStackLayout, Control>
     {
 
         int previousIndex;
@@ -121,14 +94,4 @@ public class CustomStackLayoutRenderer : VisualElementRenderer<CustomStackLayout
             wholeRowElement.BackgroundColor = Xamarin.Forms.Color.Blue;
         }
     }
-
-```
-
-## <a name="requirements-to-run-the-demo"></a>Requirements to run the demo ##
-
-* [Visual Studio 2017](https://visualstudio.microsoft.com/downloads/) or [Visual Studio for Mac](https://visualstudio.microsoft.com/vs/mac/).
-* Xamarin add-ons for Visual Studio (available via the Visual Studio installer).
-
-## <a name="troubleshooting"></a>Troubleshooting ##
-### Path too long exception
-If you are facing path too long exception when building this example project, close Visual Studio and rename the repository to short and build the project.
+}
